7b7567cedf51964731a9c9be9c82a28eb1951567
Zulkifli Raihan <zuran2907@gmail.com>
2021-12-21 21:37:25 +0700

    change Favicon

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import NavbarStyleOne from '@/components/_App/NavbarStyleOne';
-// import NavbarStyleThree from '@/components/_App/NavbarStyleThree';
+
 import Navbar from '@/components/Home/Navbar';
 import Banner from '@/components/Home/Banner';
 import About from '@/components/Home/About';
@@ -13,16 +12,6 @@ import Portofolio from '@/components/Home/Portofolio';
 import Feedbacks from '@/components/Home/Feedbacks';
 import Footer from '@/components/Home/Footer';
 
-import BestAppEver from '@/components/HomeDemo1/BestAppEver';
-import AppProgressStyle1 from '@/components/Common/AppProgressStyle1';
-import AppDownloadStyle1 from '@/components/Common/AppDownloadStyle1';
-import AppScreenshotsStyle1 from '@/components/AppScreenshots/AppScreenshotsStyle1';
-import SoftwareIntegrations from '@/components/Common/SoftwareIntegrations';
-import PricingPlanStyle1 from '@/components/PricingPlan/PricingPlanStyle1';
-import ClientFeedbackStyle1 from '@/components/Feedbacks/ClientFeedbackStyle1';
-import FreeTrialStyle1 from '@/components/Common/FreeTrialStyle1';
-import FooterStyleOne from '@/components/_App/FooterStyleOne';
- 
 const IndexPage = () => {
     return (
         <>
